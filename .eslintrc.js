d2f70e08b2c8693a19a56495b6bacfb1a11e8a07
Marek Hueckmann <marek.huckmann@creativestyle.pl>
2020-01-14 22:59:50 +0100

    Linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-use-before-define': ["error", { "functions": false }],
+    'no-use-before-define': ['error', { functions: false }],
   },
   parserOptions: {
     parser: 'babel-eslint',
